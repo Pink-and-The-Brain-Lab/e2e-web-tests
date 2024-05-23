b58dd05f8542b8ef4a03ed80c3f76f10d00195d4
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-05-23 18:33:52 -0300

    .

---
+++ cypress/support/elements/CommonElements.ts
@@ -0,0 +1,10 @@
+export const CommonElements = {
+    toastr: 'div[role=alert]',
+    h1Title: 'h1',
+    h2Title: 'h2',
+    h3Title: 'h3',
+    submitButton: 'button[type="submit"]',
+    buttonButton: 'button[type="button"]',
+    emailInput: 'input[type="email"]',
+    formButtonSubmit: 'form button[type="submit"]'
+};
