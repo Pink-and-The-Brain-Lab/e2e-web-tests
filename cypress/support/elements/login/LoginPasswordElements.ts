392f1143f2aa712848458a6a65fc11e2caeb1f80
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-05-26 12:06:18 -0300

    .

---
+++ cypress/support/elements/login/LoginPasswordElements.ts
@@ -0,0 +1,9 @@
+export const LoginPasswordElements = {
+    returnButton: '.login button[type="button"]',
+    passwordInput: 'input[type="password"]',
+    textInput: 'input[type="text"]',
+    eyeButton: '.row-form > i',
+    forgotPassword: '.login a',
+    loginButton: '.login button[type="submit"]',
+    signupButton: 'footer button[type="button"]',
+};
