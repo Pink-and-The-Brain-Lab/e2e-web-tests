9d0acbfcf20af28cfc8117be9360af97db104894
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-05-27 09:15:46 -0300

    .

---
+++ cypress/support/elements/login/NewPasswordElements.ts
@@ -0,0 +1,9 @@
+export const NewPasswordElements = {
+    inputPassword1: '.login .row-form:nth-child(1) input[type="password"]',
+    inputPassword2: '.login .row-form:nth-child(3) input[type="password"]',
+    inputText1: '.login .row-form:nth-child(1) input[type="text"]',
+    inputText2: '.login .row-form:nth-child(3) input[type="text"]',
+    eyeIcon1: '.login .row-form:nth-child(1) i',
+    eyeIcon2: '.login .row-form:nth-child(3) i',
+    submitButton: '.login button.btn-primary',
+};
