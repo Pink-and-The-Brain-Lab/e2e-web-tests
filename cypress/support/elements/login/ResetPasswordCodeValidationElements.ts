a4fb71bc6511ee71155612766ae7b98328349b4f
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-05-28 08:27:57 -0300

    .

---
+++ cypress/support/elements/login/ResetPasswordCodeValidationElements.ts
@@ -0,0 +1,8 @@
+export const ResetPasswordCodeValidationElements = {
+    input1: 'cdk-code-validation input[type="text"]:nth-child(1)',
+    input2: 'cdk-code-validation input[type="text"]:nth-child(2)',
+    input3: 'cdk-code-validation input[type="text"]:nth-child(3)',
+    input5: 'cdk-code-validation input[type="text"]:nth-child(5)',
+    input6: 'cdk-code-validation input[type="text"]:nth-child(6)',
+    input7: 'cdk-code-validation input[type="text"]:nth-child(7)',
+};
