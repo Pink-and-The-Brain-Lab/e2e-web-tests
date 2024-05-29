2c522362b2e7ccd6f5acc8c32d9eedef13804a67
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-05-29 09:29:14 -0300

    .

---
+++ cypress/support/elements/login/ResetPasswordElements.ts
@@ -0,0 +1,6 @@
+export const ResetPasswordElements = {
+    inputEmail: 'input[type="email"]',
+    sendCodeButton: 'button[type="submit"]',
+    backToLoginButton: 'button[type="button"]',
+    toastr: 'div[role=alert]',
+};
