556d7a28d99522de3dfb32f87a40acfb54b99496
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-05-30 17:41:10 -0300

    .

---
+++ cypress/support/elements/login/SignupElements.ts
@@ -0,0 +1,11 @@
+export const SignupElements = {
+    inputEmail: 'app-sign-up input[type="email"]',
+    inputFullname: 'app-sign-up input[type="text"]',
+    inputPassword: 'cdk-create-password .row-form:nth-child(1) input[type="password"]',
+    inputConfirmPassword: 'cdk-create-password .row-form:nth-child(3) input[type="password"]',
+    inputText1: 'cdk-create-password .row-form:nth-child(1) input[type="text"]',
+    inputText2: 'cdk-create-password .row-form:nth-child(3) input[type="text"]',
+    eyeIcon1: 'cdk-create-password .row-form:nth-child(1) i',
+    eyeIcon2: 'cdk-create-password .row-form:nth-child(3) i',
+    submitButton: 'form button.btn-primary',
+};
